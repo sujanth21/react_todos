6e11de6e81e27d6cee8ff2248d5f7fb5557bc037
Sujanth <sujanth1987@gmail.com>
2021-05-08 09:51:26 +1000

    initial setup for react todos app

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-size: 62.5%;
+    font-family: 'Itim', cursive;
+}
+
+h1 {
+    font-size: 4.8rem;
+}
+h2 {
+    font-size: 3.6rem;
+}
+h3 {
+    font-size: 2.4rem;
+}
+h4 {
+    font-size: 2rem;
+}
+
+h5, h6 {
+    font-size: 1.9rem;
+}
+
+p, input, li, a {
+    font-size: 1.6rem;
+}
+`;
+
+export default GlobalStyle;
